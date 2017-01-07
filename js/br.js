8d45d94a2c237a91734a66360975ad85f82dff67
brresnic <benjamin_resnick@brown.edu>
2017-01-07 13:54:53 -0600

    merry bell initial commit

--- js/br.js
+++ js/br.js
@@ -1,8 +1,28 @@
 $( document ).ready(function() {
   $( window ).resize(function() {
-    console.log($('#merry-img').height());
-    $('#main').height($('#merry-img').height());
+    console.log($('#map-img').height());
+    $('#main').height($('#map-img').height());
+  });
+  $( "#map-img" ).load(function() {
+  	$('#main').height($('#map-img').height());
   });
 
+  var duration = 740;
+  for(i = 1; i<11; i++){
+	  $('#main a:nth-child('+i.toString()+') img').animate({
+	    opacity: 1
+	  }, duration, function() {
+	    // Animation complete.
+	    if(duration == 3540){ //on last fade, fade song
+	    	$('#sail-song').animate({
+	    		opacity: 1
+	  		},1000, function() {
+	    		// Animation complete.
+  			});
+	    }
+
+	  });
+	  duration += 280;	
+  } 
 
 });
